dd4d8b4db0efaf9613eab4b33822e09250a27d92
Alison Venancio <alison.venancio@gmail.com>
2021-06-19 11:57:02 -0400

    Adding inputs in PORTB

--- include/joypad.h
+++ include/joypad.h
@@ -1,22 +1,26 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#define BUTTON_UP (PINB & (1 << PINB1))
+#define PB5_SET_OUT DDRB |= (1 << DDB5);
+#define LED_ON PORTB |= (1 << PORTB5)
+#define LED_OFF PORTB &= ~(1 << PORTB5)
+
+#define PB0_SET_IN DDRB &= ~(1 << DDB0);
+#define PB1_SET_IN DDRB &= ~(1 << DDB1);
+#define PB2_SET_IN DDRB &= ~(1 << DDB2);
+#define PB3_SET_IN DDRB &= ~(1 << DDB3);
+
+#define BUTTON_LEFT (PINB & (1 << PINB0))
 #define BUTTON_DOWN (PINB & (1 << PINB2))
-#define BUTTON_RIGHT (PINB & (1 << PINB3))
-#define BUTTON_LEFT (PINB & (1 << PINB4))
+#define BUTTON_RIGHT (PINB & (1 << PINB1))
+#define BUTTON_UP (PINB & (1 << PINB3))
 
-#define PD2_SET_IN DDRB &= ~(1 << DDB1);
-#define PD3_SET_IN DDRB &= ~(1 << DDB2);
-#define PD4_SET_IN DDRB &= ~(1 << DDB3);
-#define PD5_SET_IN DDRB &= ~(1 << DDB4);
+#define PB0_SET_INT PCMSK0 |= (1 << PCINT0);
+#define PB1_SET_INT PCMSK0 |= (1 << PCINT1);
+#define PB2_SET_INT PCMSK0 |= (1 << PCINT2);
+#define PB3_SET_INT PCMSK0 |= (1 << PCINT3);
 
-#define PD2_SET_INT PCMSK0 |= (1 << PCINT1);
-#define PD3_SET_INT PCMSK0 |= (1 << PCINT2);
-#define PD4_SET_INT PCMSK0 |= (1 << PCINT3);
-#define PD5_SET_INT PCMSK0 |= (1 << PCINT4);
-
-#define PCINT2_VECT_SET PCICR |= (1 << PCIE0);
+#define PCINT0_VECT_SET PCICR |= (1 << PCIE0);
 
 #define INT_ROUTINE ISR(PCINT0_vect)
 
